566513a290ff072b10e911a96dcfa954b84d3c3f
Manuel C Solis <manuelsolis@Manuels-Air.localdomain>
2018-09-04 17:03:49 -0400

    Buttons wrk

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,56 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h2>{{homeTeamName}}</h2>
+    <input type="text" v-model="homeTeamName">
+    <p>{{countHomeTeam}}</p>
+    <button v-on:click="addOneHomeTeam">Add 1</button>
+    <button v-on:click="removeOneHomeTeam">Remove 1</button>
+    <br>
+    <h2>{{awayTeamName}}</h2>
+    <input type="text" v-model="awayTeamName">
+    <p>{{countAwayTeam}}</p>
+    <button v-on:click="addOneAwayTeam">Add 1</button>
+    <button v-on:click="removeOneAwayTeam">Remove 1</button>
+    <br>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+    name:"count",
+    data:function(){
+        return {
+            countHomeTeam:0,
+            countAwayTeam:0,
+            homeTeamName:"Home Team Name",
+            awayTeamName:"Away Team Name",
+        }
+    },
+    methods:{
+        addOneHomeTeam:function(){
+            console.log("add 1 was clicked")
+            this.countHomeTeam++;
+        },
+        removeOneHomeTeam:function(){
+            console.log("remove 1 was clicked")
+            this.countHomeTeam-=1;
+        },
+        addOneAwayTeam:function(){
+            console.log("add 1 was clicked")
+            this.countAwayTeam++;
+        },
+        removeOneAwayTeam:function(){
+            console.log("remove 1 was clicked")
+            this.countAwayTeam-=1;
+        },
+
+    }
 }
 </script>
+
+
+
 
 <style>
 #app {
